903d38612029e863b7a6a6533734eb454de690e3
dtiradop <crubio@acsendo.com>
2021-11-08 12:06:58 -0500

    Subir primeros pasos del proyecto

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { FormsModule } from '@angular/forms';
+
+
+
+@NgModule({
+  declarations: [
+    NavComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
+  exports:[
+    NavComponent,
+    FooterComponent
+  ]
+})
+export class ComponentsModule { }
---
+++ src/app/pages/landing/landing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LandingRoutingModule } from './landing-routing.module';
+import { LandingComponent } from './landing.component';
+import { ComponentsModule } from 'src/app/components/components.module';
+
+
+@NgModule({
+  declarations: [
+    LandingComponent
+  ],
+  imports: [
+    CommonModule,
+    LandingRoutingModule,
+    ComponentsModule
+  ]
+})
+export class LandingModule { }
